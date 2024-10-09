30eaa0b98ec9a6d392fa34a3a035bf176a12c0ba
Saeed ullah <saeed.khan5032@gmail.com>
2024-10-09 23:34:19 +0500

    -> Frame MetricsTracker

--- app/src/main/java/com/sample/renderspy/MainApplication.kt
+++ app/src/main/java/com/sample/renderspy/MainApplication.kt
@@ -4,10 +4,12 @@ import android.app.Activity
 import android.app.Application
 import android.view.View
 import com.base.render.monitoring.AppTtiListener
+import com.performance.renderspy.rendering.ActivityFrameMetricsTracker
 import com.performance.renderspy.startup.AppStartupTimeTracker
 import com.performance.renderspy.tti.BaseTtiTracker
 import com.performance.renderspy.tti.ViewTtiTracker
 import com.performance.renderspy.tti.helper.ActivityTtfrHelper
+import com.sample.renderspy.monitoring.ActivityFrameMetricsListener
 import com.sample.renderspy.monitoring.AppStartupTimeListener
 
 class MainApplication : Application() {
@@ -19,7 +21,7 @@ class MainApplication : Application() {
         AppStartupTimeTracker.register(this, AppStartupTimeListener)
 //
 //        // setup rendering performance tracking
-//        ActivityFrameMetricsTracker.register(this, ActivityFrameMetricsListener)
+        ActivityFrameMetricsTracker.register(this, ActivityFrameMetricsListener)
 //
         // setup Activity TTI tracking
         ActivityTtfrHelper.register(this, viewTtiTracker)
